ee5337ec889000c630b9c1de86d2899f852f123c
Carlos Tirado <catirado87@gmail.com>
2015-11-17 20:34:32 +0100

    Restructure views and controllers showing

--- src/CorredoresRioja/UI/RunnersBundle/Controller/DefaultController.php
+++ src/CorredoresRioja/UI/RunnersBundle/Controller/DefaultController.php
@@ -13,8 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('RunnersBundle:Default:index.html.twig');
     }
 }
--- src/CorredoresRioja/UI/RunnersBundle/Controller/UsersController.php
+++ src/CorredoresRioja/UI/RunnersBundle/Controller/UsersController.php
@@ -6,16 +6,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class RacesController extends Controller
+class UsersController extends Controller
 {
     /**
-     * @Route("/carreras", name="races")
+     * @Route("/login", name="login")
      */
-    public function indexAction(Request $request)
+    public function loginAction(Request $request)
     {
-        return $this->render('RunnersBundle:Races:index.html.twig');
-        /*return $this->render('default/races.html.twig', array(
+        return $this->render('default/races.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));*/
+        ));
     }
 }
+
