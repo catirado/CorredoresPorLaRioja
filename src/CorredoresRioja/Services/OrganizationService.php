4b52731f444290b83bfc8a82fc6f3c0fe4cb3149
Carlos Tirado <catirado87@gmail.com>
2015-11-17 20:33:54 +0100

    Create services

--- src/CorredoresRioja/Services/OrganizationService.php
+++ src/CorredoresRioja/Services/OrganizationService.php
@@ -3,5 +3,7 @@
 namespace CorredoresRioja\Services;
 
 class OrganizationService {
-    //put your code here
+  
+    //si cambio el nombre de una carreara tengo que asegurarme que no exista otra con la misma salt
+    
 }
