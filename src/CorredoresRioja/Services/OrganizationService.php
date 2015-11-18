a200bb76c62418146aeaa01541f0b744e914e08e
Carlos Tirado <catirado87@gmail.com>
2015-11-18 17:56:39 +0100

    We remove services layer to simplify

--- src/CorredoresRioja/Services/OrganizationService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CorredoresRioja\Services;
-
-class OrganizationService {
-  
-    //si cambio el nombre de una carreara tengo que asegurarme que no exista otra con la misma salt
-    
-}
