f438b869351913f83cca2c4d24825182c6b33c7c
Carlos Tirado <catirado87@gmail.com>
2015-11-18 09:59:53 +0100

    Add empty test

---
+++ src/CorredoresRioja/Domain/Tests/RaceTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CorredoresRioja\Domain\Tests;
+
+class RaceTest extends \PHPUnit_Framework_TestCase{
+    
+    public function testValidateSlug()
+    {
+        
+    }
+    
+    //for more cases
+    public function testIsInscriptionsOpen()
+    {
+        
+    }
+}
