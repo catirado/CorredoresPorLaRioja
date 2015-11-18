b27e6965304c7a263a0c03a7bedad588f0ff9a64
Carlos Tirado <catirado87@gmail.com>
2015-11-18 09:29:53 +0100

    Create UserInterface and provider for Runner

---
+++ src/CorredoresRioja/Infrastructure/Security/RunnerUser.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CorredoresRioja\Infrastructure\Security;
+use Symfony\Component\Security\Core\User\UserInterface; 
+use Symfony\Component\Security\Core\User\EquatableInterface;
+
+class RunnerUser implements UserInterface, EquatableInterface{
+   
+    private $username;
+    private $password;
+    private $salt;
+    private $roles;
+    
+    public function __construct($username, $password, $salt) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->salt = $salt;
+        $this->roles = array('ROLE_RUNNER');
+    }
+    
+    public function eraseCredentials() {
+        
+    }
+    
+    function getUsername() {
+        return $this->username;
+    }
+    
+    function getPassword()
+    {
+        return $this->password;
+    }
+    
+    function getSalt()
+    {
+        return $this->salt;
+    }
+    
+    function getRoles()
+    {
+        return array('ROLE_RUNNER');
+    }
+    
+    public function isEqualTo(UserInterface $user) {
+        if(!$user instanceof  RunnerUser ||
+                $this->password !== $user->getPassword() ||
+                $this->salt !== $user->getSalt() ||
+                $this->username !== $user->getUsername())
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/CorredoresRioja/Infrastructure/Security/RunnerUserProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CorredoresRioja\Infrastructure\Security;
+use Symfony\Component\Security\Core\User\UserProviderInterface; 
+use Symfony\Component\Security\Core\User\UserInterface; 
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException; 
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException; 
+use CorredoresRioja\Domain\Repositories\RunnerRepository;
+use CorredoresRioja\Infrastructure\Security\RunnerUser;
+
+class RunnerUserProvider implements UserProviderInterface{
+    
+    private $runnerRepository;
+    
+    public function __construct(RunnerRepository $repository) {
+        $this->runnerRepository = $repository;
+    }
+    
+    public function loadUserByUsername($username) {
+        $userData = $this->runnerRepository->getRunnerByDNI($username);
+        if ($userData){ 
+            $password = $userData->getPassword(); 
+            $salt = $userData->getSalt(); 
+            return new​ CorredorUser($username, $password,$salt); 
+        } 
+        
+        throw new UsernameNotFoundException(
+                sprinf('No existe un usuario con DNI "%s".', $username)
+        );
+    }
+    
+    public function refreshUser(UserInterface $user) {
+        if (!$user instanceof RunnerUser){
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s are not supported', get_class($user))
+            );
+        }
+        
+        return $this->loadUserByUsername($user->getUsername());
+    }
+    
+    public function supportsClass($class) {
+        return $class == 'CorredoresRioja\Infrastructure\Security\RunnerUser';
+    }
+    
+    
+}
