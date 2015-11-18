fab8534d56b6e1c369540a9b71a3c7696c45d7e7
Carlos Tirado <catirado87@gmail.com>
2015-11-18 09:28:59 +0100

    remove comments and add namespaces in mailer services

--- src/CorredoresRioja/Infrastructure/Mail/MailerService.php
+++ src/CorredoresRioja/Infrastructure/Mail/MailerService.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace CorredoresRioja\Infrastructure\Mail;
 
-/**
- * Description of MailerService
- *
- * @author Carlost
- */
 interface MailerService {
     //put your code here
 }
