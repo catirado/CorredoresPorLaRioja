f619c4e7c690e0d8b1fa36b8fb646a47b8c17afb
Carlos Tirado <catirado87@gmail.com>
2015-11-18 10:27:05 +0100

    Create OrganizationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             //we add here all the bundles
             //new AppBundle\AppBundle(),
             new CorredoresRioja\UI\RunnersBundle\RunnersBundle(),
+            new CorredoresRioja\UI\OrganizationsBundle\OrganizationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/CorredoresRioja/UI/OrganizationsBundle/OrganizationsBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CorredoresRioja\UI\OrganizationsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OrganizationsBundle extends Bundle {
+
+}
+
