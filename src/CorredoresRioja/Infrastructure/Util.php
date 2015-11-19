4a39199fe501d9a08f1cfd09ebd9b3a3a40cc775
Carlos Tirado <catirado87@gmail.com>
2015-11-19 10:55:05 +0100

    Fix method

--- src/CorredoresRioja/Infrastructure/Util.php
+++ src/CorredoresRioja/Infrastructure/Util.php
@@ -3,15 +3,14 @@
 namespace CorredoresRioja\Infrastructure;
 
 class Util {
-    //put your code here
-    //get slug
-    static public function getSlug($cadena, $separador = '-') ​ 
-    { 
+    
+    static public function getSlug($cadena, $separador='')
+    {
         // Código copiado de http://cubiq.org/the-perfect-php-clean-url-generator 
-        $slug = iconv( ​ 'UTF-8' ​ , ​   ​ 'ASCII//TRANSLIT' ​ , ​  $cadena ​ ); 
-        $slug = preg_replace ​ ( ​ "/[^a-zA-Z0-9\/_|+ -]/" ​ , ​   ​ '' ​ , ​  $slug ​ ); 
-        $slug​ = strtolower ​ ( ​ trim ​ ( ​ $slug ​ , ​  $separador ​ )); 
-        $slug​ = preg_replace ​ ( ​ "/[\/_|+ -]+/" ​ , ​  $separador ​ , ​  $slug ​ ); 
+        $slug = iconv('UTF-8','ASCII//TRANSLIT',$cadena); 
+        $slug = preg_replace("/[^a-zA-Z0-9\/_|+ -]/",'',$slug); 
+        $slug​ = strtolower(trim($slug,$separador)); 
+        $slug​ = preg_replace("/[\/_|+ -]+/",$separador,$slug); 
         return $slug; 
     }
 }
